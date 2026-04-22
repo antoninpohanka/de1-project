2d9cd47e2e63b6f82ccd162c552a41a54a3c96b5
antoninpohanka <270990@vutbr.cz>
2026-04-22 10:52:48 +0200

    formatting

--- stopwatch/stopwatch.srcs/sources_1/new/display_driver.vhd
+++ stopwatch/stopwatch.srcs/sources_1/new/display_driver.vhd
@@ -3,12 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity display_driver is
-    Port ( clk : in STD_LOGIC;
-           rst : in STD_LOGIC;
+    Port ( clk    : in STD_LOGIC;
+           rst    : in STD_LOGIC;
            data_i : in STD_LOGIC_VECTOR (31 downto 0);
-           seg_o : out STD_LOGIC_VECTOR (6 downto 0);
-           dp_o : out STD_LOGIC;
-           an_o : out STD_LOGIC_VECTOR (7 downto 0));
+           seg_o  : out STD_LOGIC_VECTOR (6 downto 0);
+           dp_o   : out STD_LOGIC;
+           an_o   : out STD_LOGIC_VECTOR (7 downto 0));
 end display_driver;
 
 architecture Behavioral of display_driver is
