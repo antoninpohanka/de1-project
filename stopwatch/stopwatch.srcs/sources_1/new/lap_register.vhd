c21db05c2ba5494b93ad998f2bf35a30bff4f9f0
kosek-luk <270825@vutbr.cz>
2026-04-23 11:34:03 +0200

    Add VHDL file headers

--- stopwatch/stopwatch.srcs/sources_1/new/lap_register.vhd
+++ stopwatch/stopwatch.srcs/sources_1/new/lap_register.vhd
@@ -1,3 +1,22 @@
+----------------------------------------------------------------------------------
+-- Company:      Brno University of Technology
+-- Engineer:     Antonin Pohanka
+-- 
+-- Create Date:  04/09/2026 08:22:45 AM
+-- Design Name:  Stopwatch with Lap Time Function
+-- Module Name:  lap_register - Behavioral
+-- Project Name: Digital Electronics 1 Project - Stopwatch
+-- Target Devices: Nexys A7-50T (Artix-7)
+-- Tool Versions: Vivado 2020.1 (or newer)
+-- Description:  Memory register that stores the lap time. It continuously 
+--               tracks the current running time and freezes the captured 
+--               value as soon as the lap mode flag is asserted.
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments: Project for DE1 course at FEEC BUT.
+-- 
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
