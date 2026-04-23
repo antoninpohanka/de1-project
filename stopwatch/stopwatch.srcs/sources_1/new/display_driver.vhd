4bad7706ad6312ebcf240912c9bc3ef721da0e05
kosek-luk <270825@vutbr.cz>
2026-04-23 11:35:22 +0200

    Add project header comments to display_driver.vhd

--- stopwatch/stopwatch.srcs/sources_1/new/display_driver.vhd
+++ stopwatch/stopwatch.srcs/sources_1/new/display_driver.vhd
@@ -1,3 +1,24 @@
+----------------------------------------------------------------------------------
+-- Company:      Brno University of Technology
+-- Engineer:     Antonin Pohanka
+-- 
+-- Create Date:  04/09/2026 08:22:45 AM
+-- Design Name:  Stopwatch with Lap Time Function
+-- Module Name:  display_driver - Behavioral
+-- Project Name: Digital Electronics 1 Project - Stopwatch
+-- Target Devices: Nexys A7-50T (Artix-7)
+-- Tool Versions: Vivado 2020.1 (or newer)
+-- Description:  Multiplexing 8-digit 7-segment display driver. It cycles 
+--               through individual digits at approximately 762 Hz, decoding 
+--               4-bit hex inputs into 7-segment active-LOW cathode signals. 
+--               It also manages decimal points for time formatting and supports 
+--               custom characters (blank and 'L' for Lap mode).
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments: Project for DE1 course at FEEC BUT.
+-- 
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
