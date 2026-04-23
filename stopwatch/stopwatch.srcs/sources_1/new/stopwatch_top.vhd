e6fd8b87290d7e6f4bace946d8ec9983b6328d7d
kosek-luk <270825@vutbr.cz>
2026-04-23 11:10:34 +0200

    Revise header comments in stopwatch_top.vhd
    Updated header comments with project details and author information.

--- stopwatch/stopwatch.srcs/sources_1/new/stopwatch_top.vhd
+++ stopwatch/stopwatch.srcs/sources_1/new/stopwatch_top.vhd
@@ -1,20 +1,23 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company:      Brno University of Technology
+-- Engineer:     Antonin Pohanka
 -- 
--- Create Date: 04/09/2026 08:22:45 AM
--- Design Name: 
--- Module Name: stopwatch_top - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- Create Date:  04/09/2026 08:22:45 AM
+-- Design Name:  Stopwatch with Lap Time Function
+-- Module Name:  stopwatch_top - Behavioral
+-- Project Name: Digital Electronics 1 Project - Stopwatch
+-- Target Devices: Nexys A7-50T (Artix-7)
+-- Tool Versions: Vivado 2020.1 (or newer)
+-- Description:  Top-level entity for digital stopwatches. 
+--               Features: start/stop/reset, lap time measurement, 
+--               and display on 7-segment LED.
 -- 
--- Dependencies: 
+-- Dependencies: clock_enable.vhd, cnt_up_down.vhd, hex_7seg.vhd, 
+--               driver_7seg_4digits.vhd (or similar based on your structure)
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments:
+-- Additional Comments: Project for DE1 course at FEEC BUT.
 -- 
 ----------------------------------------------------------------------------------
 
